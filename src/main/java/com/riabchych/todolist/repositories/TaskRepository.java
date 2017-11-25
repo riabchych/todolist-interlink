2062386107a7112bccc57569395c954554cc2743
Yevhenii Riabchych <riabchych@gmail.com>
2017-11-25 23:07:17 +0200

    рефакторинг

--- src/main/java/com/riabchych/todolist/repositories/TaskRepository.java
+++ src/main/java/com/riabchych/todolist/repositories/TaskRepository.java
@@ -1,12 +1,10 @@
 package com.riabchych.todolist.repositories;
 
 import com.riabchych.todolist.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    Iterable<Task> findTaskByDate(LocalDate date);
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    Task findById(long taskId);
 }
