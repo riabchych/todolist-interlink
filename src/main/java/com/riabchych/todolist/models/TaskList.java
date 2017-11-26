0374193e1b3e6b138131dfb7e7ca1a8a083dff79
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-26 16:54:15 +0200

    добавлена модель списка

---
+++ src/main/java/com/riabchych/todolist/models/TaskList.java
@@ -0,0 +1,50 @@
+package com.riabchych.todolist.models;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "lists")
+public class TaskList implements Serializable {
+
+    @Id
+    @Column()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column()
+    @NotBlank
+    private String title;
+
+    @Column()
+    @NotBlank
+    private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
