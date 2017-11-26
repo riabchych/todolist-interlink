e408e3c7eb500f6df7821d57842a0683ae3c0b60
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-26 16:50:24 +0200

    рефакторинг

--- src/main/java/com/riabchych/todolist/models/Priority.java
+++ src/main/java/com/riabchych/todolist/models/Priority.java
@@ -1,8 +1,22 @@
 package com.riabchych.todolist.models;
 
 public enum Priority {
-    LOW,
-    MEDIUM,
-    HIGH,
-    IMPORTANT
+    LOW(1), MEDIUM(2), HIGH(3);
+    private int value;
+
+    Priority(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Priority getValueI() {
+        return this;
+    }
 }
