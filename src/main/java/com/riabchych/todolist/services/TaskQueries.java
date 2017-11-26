b02909fd5e1825d5ea9a50dcc74fbec6c512839d
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-26 16:59:27 +0200

    добавлены новые методы для работы с заданиями

--- src/main/java/com/riabchych/todolist/services/TaskQueries.java
+++ src/main/java/com/riabchych/todolist/services/TaskQueries.java
@@ -1,5 +1,21 @@
 package com.riabchych.todolist.services;
 
+import com.riabchych.todolist.models.Priority;
+import com.riabchych.todolist.models.Status;
+import com.riabchych.todolist.models.Task;
+
 public interface TaskQueries {
-    Iterable getAllTasks();
+    Iterable<Task> getAllTasks();
+
+    Iterable<Task> filterByPriority(Priority priority);
+
+    Iterable<Task> getAllTasksOrderByPriority();
+
+    Iterable<Task> getAllTasksOrderByStatus();
+
+    Iterable<Task> filterByCompleted(boolean isCompleted);
+
+    Task changeTaskPriority(long id, Priority priority);
+
+    Task changeTaskStatus(long id, Status status);
 }
