7bd197d937f18a3dd281956eda5125e2e70a91f9
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-26 16:55:07 +0200

    добавлен новые методы

--- src/main/java/com/riabchych/todolist/repositories/TaskRepository.java
+++ src/main/java/com/riabchych/todolist/repositories/TaskRepository.java
@@ -7,4 +7,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
     Task findById(long taskId);
+
+    Iterable<Task> findTasksByPriority(int priority);
+
+    Iterable<Task> findAllByOrderByPriorityAsc();
+
+    Iterable<Task> findAllByOrderByStatusAsc();
+
+    Iterable<Task> findTasksByIsCompleted(boolean isCompleted);
 }
