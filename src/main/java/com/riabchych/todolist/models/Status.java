08fae0eccd35c1c6a26a48af0f0b0381309c4b4a
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-26 16:52:14 +0200

    рефакторинг

--- src/main/java/com/riabchych/todolist/models/Status.java
+++ src/main/java/com/riabchych/todolist/models/Status.java
@@ -1,7 +1,22 @@
 package com.riabchych.todolist.models;
 
 public enum Status {
-    PENDING,
-    COMPLETED,
-    PAUSED
+    PENDING(1), PAUSED(2), COMPLETED(3);
+    private int value;
+
+    Status(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Status getValueI() {
+        return this;
+    }
 }
